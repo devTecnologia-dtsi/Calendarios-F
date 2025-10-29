33570053802cdb0112b20c941167802984f647c7
andreslopezuni <andres.lopez.m@uniminuto.edu>
2025-10-29 15:22:16 -0500

    Se ajusta las variables de entorno del ambiente de produccion y QA

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -8,7 +8,6 @@ export const environment = {
 
     // Configuración de MSAL para autenticación con Azure AD
     clientId: '0ffbc27e-5a3a-4b6b-bcd3-8f06486ecf03',
-    // clientId: 'd72569ba-95a4-4dd7-b0d2-f9d6d4d6a223',
     authority: 'https://login.microsoftonline.com/b1ba85eb-a253-4467-9ee8-d4f8ed4df300',
     redirectUri: 'https://testcalendarios.uniminuto.edu/login'
 };
