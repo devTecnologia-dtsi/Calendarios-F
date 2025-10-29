debb698b4443432f9cb99f3d264a256789a227b4
andreslopezuni <andres.lopez.m@uniminuto.edu>
2025-10-29 15:10:35 -0500

    Se ajusta el redirect URI de calendarion en el ambiente de produccion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,5 +9,5 @@ export const environment = {
     // Configuración de MSAL para autenticación con Azure AD
     clientId: 'a01195af-ceac-41e3-8a2c-1274b0181b1f',
     authority: 'https://login.microsoftonline.com/b1ba85eb-a253-4467-9ee8-d4f8ed4df300',
-    redirectUri: 'https://testcalendarios.uniminuto.edu/login'
+    redirectUri: 'https://calendarios.uniminuto.edu/login'
 };
